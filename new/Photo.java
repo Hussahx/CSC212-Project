b586c7b97d0cae4922681cbf21709a0089ac7811
Mayaz-15 <mayaz9995@gmail.com>
2025-04-25 13:40:32 +0300

    Add files via upload

--- new/Photo.java
+++ new/Photo.java
@@ -1,60 +1,58 @@
+package datastructur;
 public class Photo {
     
-    private String path;
-    LinkedList<String> allTags = new LinkedList<>();
-
-   // Constructor
-   public Photo(String path, LinkedList<String> tags) {
-       this.path = path;
-
-       if (! tags.empty()) {   
-          tags.findFirst();
-          while ( !tags.last()) { //all except the last 
-            allTags.insert(tags.retrieve());
-            tags.findNext();
-          }
-          allTags.insert(tags.retrieve());//the last one
-        }
-    }
+          private String path;
+          LinkedList<String> allTags = new LinkedList<>();
          
-    // Return the path (full file name) of the photo. A photo is uniquely identified by its path.
-    public String getPath() {
-        return path;
-    }
+        // Constructor
+         public Photo(String path, LinkedList<String> tags)
+         {
+             this.path = path;
+                
+             if (! tags.empty())
+             {   
+                tags.findFirst();
+                while ( !tags.last())//all except the last 
+                {
+                    allTags.insert(tags.retrieve());
+                    tags.findNext();
+                }
+                allTags.insert(tags.retrieve());//the last one
+             }
+        }
+         
+        // Return the path (full file name) of the photo. A photo is uniquely identified by its path.
+        public String getPath()
+        {
+                return path;
+        }
         
-    // Return all tags associated with the photo
-    /*public LinkedList<String> getTags() {
-        LinkedList<String> tagsNew = new LinkedList<String>();
-        if (! allTags.empty()) {
-            allTags.findFirst();
-            while ( ! allTags.last()) {
+        // Return all tags associated with the photo
+        public LinkedList<String> getTags()
+        {
+            LinkedList<String> tagsNew = new LinkedList<String>();
+            
+            if (! allTags.empty())
+            {
+                allTags.findFirst();
+                while ( ! allTags.last())
+                {
+                    tagsNew.insert(allTags.retrieve());
+                    allTags.findNext();
+                }
                 tagsNew.insert(allTags.retrieve());
-                allTags.findNext();
             }
-            tagsNew.insert(allTags.retrieve());
+            return tagsNew;
         }
-        return tagsNew;
-    }*/
-  
-    public LinkedList<String> getTags() {
-        LinkedList<String> tagsNew = new LinkedList<String>();
-        if (!allTags.empty()) {
-            for (allTags.findFirst(); ; allTags.findNext()) {
-                tagsNew.insert(allTags.retrieve());
-                if (allTags.last()) break;
-            }
-        }
-        return tagsNew;
-    }
- 
-    
+
     @Override
     public String toString() {
         
         String str = "Photo{" + "path=" + path + ", allTags=" ;
         
         allTags.findFirst();
-        while ( ! allTags.last()) {
+        while ( ! allTags.last())
+        {
             str += allTags.retrieve().toString() + "; ";
             allTags.findNext();
         }
