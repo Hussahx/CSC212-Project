fbd1c9c92291ac88eee09091d4a00aa41567d552
Hussahx <161141145+Hussahx@users.noreply.github.com>
2025-04-26 01:28:59 +0300

    Update Photo.java

--- new/Photo.java
+++ new/Photo.java
@@ -1,58 +1,60 @@
-package datastructur;
 public class Photo {
     
-          private String path;
-          LinkedList<String> allTags = new LinkedList<>();
-         
-        // Constructor
-         public Photo(String path, LinkedList<String> tags)
-         {
-             this.path = path;
-                
-             if (! tags.empty())
-             {   
-                tags.findFirst();
-                while ( !tags.last())//all except the last 
-                {
-                    allTags.insert(tags.retrieve());
-                    tags.findNext();
-                }
-                allTags.insert(tags.retrieve());//the last one
-             }
-        }
-         
-        // Return the path (full file name) of the photo. A photo is uniquely identified by its path.
-        public String getPath()
-        {
-                return path;
-        }
-        
-        // Return all tags associated with the photo
-        public LinkedList<String> getTags()
-        {
-            LinkedList<String> tagsNew = new LinkedList<String>();
-            
-            if (! allTags.empty())
-            {
-                allTags.findFirst();
-                while ( ! allTags.last())
-                {
-                    tagsNew.insert(allTags.retrieve());
-                    allTags.findNext();
-                }
-                tagsNew.insert(allTags.retrieve());
-            }
-            return tagsNew;
-        }
+    private String path;
+    LinkedList<String> allTags = new LinkedList<>();
 
+   // Constructor
+   public Photo(String path, LinkedList<String> tags) {
+       this.path = path;
+
+       if (! tags.empty()) {   
+          tags.findFirst();
+          while ( !tags.last()) { //all except the last 
+            allTags.insert(tags.retrieve());
+            tags.findNext();
+          }
+          allTags.insert(tags.retrieve());//the last one
+        }
+    }
+         
+    // Return the path (full file name) of the photo. A photo is uniquely identified by its path.
+    public String getPath() {
+        return path;
+    }
+        
+    // Return all tags associated with the photo
+    /*public LinkedList<String> getTags() {
+        LinkedList<String> tagsNew = new LinkedList<String>();
+        if (! allTags.empty()) {
+            allTags.findFirst();
+            while ( ! allTags.last()) {
+                tagsNew.insert(allTags.retrieve());
+                allTags.findNext();
+            }
+            tagsNew.insert(allTags.retrieve());
+        }
+        return tagsNew;
+    }*/
+  
+    public LinkedList<String> getTags() {
+        LinkedList<String> tagsNew = new LinkedList<String>();
+        if (!allTags.empty()) {
+            for (allTags.findFirst(); ; allTags.findNext()) {
+                tagsNew.insert(allTags.retrieve());
+                if (allTags.last()) break;
+            }
+        }
+        return tagsNew;
+    }
+ 
+    
     @Override
     public String toString() {
         
         String str = "Photo{" + "path=" + path + ", allTags=" ;
         
         allTags.findFirst();
-        while ( ! allTags.last())
-        {
+        while ( ! allTags.last()) {
             str += allTags.retrieve().toString() + "; ";
             allTags.findNext();
         }
